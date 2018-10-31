026b12d2172e0daebf0f7468dc81ceeff5129111
Hendi Santika <hendisantika@gmail.com>
2018-10-31 07:12:28 +0700

    #1 - Initial Project

---
+++ src/main/java/com/hendisantika/springbootprometheusgrafana/SpringBootPrometheusGrafanaApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootprometheusgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrometheusGrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPrometheusGrafanaApplication.class, args);
+    }
+}
